37a253c830b4daea26bd5c4f75da82861fca1744
Lucas Domingues <52726171+LucasVD23@users.noreply.github.com>
2021-09-04 14:58:56 -0300

    Corrige envio de dados para passos 1 e 3

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ def tratar_ping(dados):
     if(len(args)==1):
         dados_env = b':server PONG server :\r\n'
     else:
-        dados_env = b':server PONG server :' + args[1] + b'\r\n'
+        dados_env = b':server PONG server :' + args[1] + b'\n'
     return dados_env
 
 def tratar_nick_valido(servidor, conexao, nick):
@@ -13,7 +13,8 @@ def tratar_nick_valido(servidor, conexao, nick):
         dados_env = f":server 433 {apelido_atual.decode()} {nick.decode()} :Nickname is already in use\r\n".encode()
     elif conexao.is_initial_nick():
         servidor.mudar_nick_conexao(conexao, nick)
-        dados_env = f":server 001 {nick.decode()} :Welcome\r\n".encode()    
+        conexao.enviar(f":server 001 {nick.decode()} :Welcome\r\n".encode())
+        dados_env = f":server 422 {nick.decode()} :MOTD File is missing\r\n".encode()    
     else:
         servidor.mudar_nick_conexao(conexao, nick)
         dados_env = f":{conexao.get_nick().decode()} NICK {nick.decode()}\r\n".encode()
